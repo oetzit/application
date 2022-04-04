e32942183e6a99af9a6164a07a2f5fe236cc2bdb
Paolo Brasolin <paolo.brasolin@eurac.edu>
2022-04-04 17:39:32 +0200

    feat: #be add timestamps everywhere in db

---
+++ backend/migrations/20220404144303_timestamps.ts
@@ -0,0 +1,17 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  knex.schema
+    .alterTable("words", (table) => table.timestamps())
+    .alterTable("games", (table) => table.timestamps())
+    .alterTable("clues", (table) => table.timestamps())
+    .alterTable("shots", (table) => table.timestamps());
+}
+
+export async function down(knex: Knex): Promise<void> {
+  knex.schema
+    .alterTable("words", (table) => table.dropTimestamps())
+    .alterTable("games", (table) => table.dropTimestamps())
+    .alterTable("clues", (table) => table.dropTimestamps())
+    .alterTable("shots", (table) => table.dropTimestamps());
+}
